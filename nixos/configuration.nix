06f4e9b770b1541727a4658b467e0ec9fdcda8f1
theendercore <theendercore@gmail.com>
2024-11-05 21:40:51 +0200

    sddm wayland [44 current 2024-11-05 21:40:48]

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,7 +60,7 @@
   };
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Enable CUPS to print documents.
