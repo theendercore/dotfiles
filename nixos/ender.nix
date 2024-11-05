19a3faed3c1ff55fae1da698ef534dbd6f3ac77e
theendercore <theendercore@gmail.com>
2024-11-05 20:38:54 +0200

    added kitty [40 current 2024-11-05 20:38:44]

--- nixos/ender.nix
+++ nixos/ender.nix
@@ -37,6 +37,9 @@
       update = "~/dotfiles/rebuild.sh";
     };
   };
+  programs.kitty = {
+    enable = true;
+  };
 
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
