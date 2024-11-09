f9beb8768ef8b182f271ac73e38573e7a3fd7e2f
theendercore <theendercore@gmail.com>
2024-11-09 14:01:38 +0200

    added intlij fix [106 current 2024-11-09 14:01:32]

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,6 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
+  lib,
   config,
   pkgs,
   inputs,
@@ -161,4 +162,10 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  environment.sessionVariables = {
+    LD_LIBRARY_PATH = lib.makeLibraryPath [
+      pkgs.glfw
+    ];
+  };
 }
